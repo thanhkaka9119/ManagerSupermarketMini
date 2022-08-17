66b292dec0226cec20697e18c23d60705f172c72
thanh <thanhkaka9119@gmail.com>
2022-08-17 23:12:03 +0700

    create api product

---
+++ src/main/java/com/vnpt/ManagerSupermarketMiniApplication.java
@@ -0,0 +1,13 @@
+package com.vnpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerSupermarketMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerSupermarketMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vnpt/ManagerSupermarketMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerSupermarketMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
